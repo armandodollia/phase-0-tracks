0c75845ea1d2f4af652aad4d770857d8bd431781
Armando Dollia <dollia.armando@gmail.com>
2017-01-09 19:48:19 -0600

    Creates initial user input prompt and data conversion

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -19,4 +19,24 @@ client_info = {
   is_colorblind: nil
 }
 
+#Ask for user input and convert to appropriate value
+puts "What is the client's name?"
+client_info[:name] = gets.chomp.to_s
+
+puts "What is the client's age?"
+client_info[:age] = gets.chomp.to_i
+
+puts "How many children does the client have?"
+client_info[:number_of_children] = gets.chomp.to_i
+
+puts "What decor theme does the client prefer?"
+client_info[:decor_theme] = gets.chomp.to_s
+
+puts "Is the client colorblind?"
+if gets.chomp.to_s == "yes"
+  client_info[:is_colorblind] = true
+else
+  client_info[:is_colorblind] = false
+end
+
 p client_info
