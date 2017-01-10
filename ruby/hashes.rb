55c6f5458276f0df4165afeec5b3c7ad826bcf1c
Armando Dollia <dollia.armando@gmail.com>
2017-01-09 19:39:10 -0600

    Creates hash object

---
+++ ruby/hashes.rb
@@ -0,0 +1,22 @@
+#Ask user to input each type of data
+
+#create a hash for the expected data
+#convert and store the data in the hash using appropriate functions
+
+#print hash to the user
+#ask the user if corrections are necessary
+
+#construct if else block to parse user input and update the hash
+#print latest hash
+
+
+#create hash object
+client_info = {
+  name: nil,
+  age: nil,
+  number_of_children: nil,
+  decor_theme: nil,
+  is_colorblind: nil
+}
+
+p client_info
