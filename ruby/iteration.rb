36d4a6ddf32b5f226eb69b54b5803f1045cd3fcc
Armando Dollia <dollia.armando@gmail.com>
2017-01-12 13:55:42 -0600

    Completes assignment

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -35,24 +35,29 @@ p student_data_array
 
 grades = [80, 70, 59, 63, 45, 98, 95, 75, 23, 5, 0]
 
+#deletes all scores bigger than or equal to 70 (Modifies original array thus needs .dup)
 def failing_grades (student_grades)
 	student_grades.dup.delete_if {|score| score >= 70}
 end
 
 p failing_grades(grades)
 
+#keeps all items smaller than or equal to 70
+# and discards the rest(modifies original array thus the need for .dup)
 def passing_grades(student_grades)
 	student_grades.dup.keep_if {|score| score >= 70}
 end
 
 p passing_grades(grades)
 
+#selects all elements in hash smaller than or equal to 36 and outputs separate array
 def odd_grades(student_grades)
 	student_grades.select {|score| score.odd?}
 end
 
 p odd_grades(grades)
 
+#drops all scores until it finds an odd number
 def reject_grades(student_grades)
 	student_grades.drop_while {|score| score.even?}
 end
@@ -69,20 +74,30 @@ pant_inch_sizes = {
 	xxl: 40
 }
 
+#removes all items smaller than or equal to 36 (Modifies original hash thus needing .dup)
 def bigger_than_large (pant_inch_sizes)
 	pant_inch_sizes.dup.delete_if {|size, circumference| circumference <= 36}
 end
 
 p bigger_than_large(pant_inch_sizes)
 
+#keeps all items smaller than 36 and discards the rest(modifies original hash thus the need for .dup)
 def smaller_than_large (pant_inch_sizes)
 	pant_inch_sizes.dup.keep_if {|size, circumference| circumference < 36}
 end
 
 p smaller_than_large(pant_inch_sizes)
 
+#selects all elements in hash smaller than or equal to 36 and outputs separate hash
 def smaller_than_xl (pant_inch_sizes)
 	pant_inch_sizes.select {|size, circumference| circumference <= 36}
 end
 
 p smaller_than_xl(pant_inch_sizes)
+
+#No way to add conditional break statement with hashes, reused #delete_if statement
+def bigger_than_small (pant_inch_sizes)
+	pant_inch_sizes.dup.delete_if {|size, circumference| circumference > 32}
+end
+
+p bigger_than_small(pant_inch_sizes)
