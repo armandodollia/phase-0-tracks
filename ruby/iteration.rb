82dffebdc1c4b7090c7c790eb66ab2e83fd0ed62
Armando Dollia <dollia.armando@gmail.com>
2017-01-12 12:06:19 -0600

    Adds .map and .map! functions to the data structures

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -11,3 +11,22 @@ student_info = {
 student_names.each {|student| puts "The student's name is #{student}"}
 
 student_info.each {|student_data, value| puts "#{student_data} = #{value}"}
+
+capital_names = student_names.map do |student|
+	student.upcase
+end
+
+p capital_names
+p student_names
+
+student_names.map! do |student|
+	student.upcase
+end
+
+p student_names
+
+student_data_array = student_info.map do |student_data, value|
+	student_data.to_s.upcase
+end
+
+p student_data_array
