ea2b24ff68d872e3d88c69322e0910d01fe8d25e
Armando Dollia <dollia.armando@gmail.com>
2017-01-13 15:08:08 -0600

    Creates nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,23 @@
+#Build stadium hash
+	#add locker room array
+		#add items in locker room array
+	#add manager's office array
+		#add things in manager's office
+	#add bathrooms array
+		#add objects in array
+	#add stands hash
+		#add sections to stands using array
+		#add people and seats to sections
+
+#BUILD HASH
+
+stadium = {
+	"Locker Room" => ['Benches', 'Lockers', 'Towels'],
+	"Manager's office" => ['Desk', 'Computer', 'Counch', 'Desk Chair', 'Filing Cabinet'],
+	"Bathroom" => ['Faucets', 'Stalls', 'Urinals', 'Toilets', 'Sinks'],
+	"stands" => {
+		"Section 1" => ['Seats', 'Fans', 'Beer', 'Burgers'],
+		"Section 2" => ['Seats', 'Fans', 'Beer', 'Hot Dogs'],
+		"Section 3" => ['Seats', 'Fans', 'Hot Dogs', 'Burgers']
+	}
+}
