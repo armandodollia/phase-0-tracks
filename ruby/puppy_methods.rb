40eb6e83c50604857cece113f7e876ee23c953f3
Armando Dollia <dollia.armando@gmail.com>
2017-01-17 22:39:01 -0600

    Implements speak method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,9 +5,16 @@ class Puppy
     toy
   end
 
+  #Define speak method and iterate using times class
+  def speak(repetitions)
+    repetitions.times {puts "Woof!"}
+  end
+
 end
 
 
 #Driver Code
 fido = Puppy.new
 fido.fetch("bone")
+
+fido.speak(3)
