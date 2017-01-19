ded9f3a1efa06dfddda65b2e5f86d88e4e7b0fac
Armando Dollia <dollia.armando@gmail.com>
2017-01-19 17:43:53 -0600

    Populates comments and corrects mistaken class input, also cleans up santa generation loop appropriatelly

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,13 +1,14 @@
 class Santa
+  #create getter and setter methods
   attr_reader :age, :ethnicity
   attr_accessor :gender
 
-  def initialize(gender, ethnicity, age)
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
     @gender = gender
     @ethnicity = ethnicity
     @raindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = age.to_i
+    @age = 0
   end
 
   def speak
@@ -18,12 +19,14 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
+  #Increase age by 1
   def celebrate_birthday
     @age += 1
   end
 
-  def get_mad_at(reindeer)
-    @raindeer_ranking.insert(@raindeer_ranking.length.to_i - 1, @raindeer_ranking.delete_at(@raindeer_ranking.index(reindeer)))
+  #Moves bad reindeer to the last spot in the array
+  def get_mad_at(bad_reindeer)
+    @raindeer_ranking.insert(@raindeer_ranking.length.to_i - 1, @raindeer_ranking.delete_at(@raindeer_ranking.index(bad_reindeer)))
   end
 
 end
@@ -33,9 +36,16 @@ end
 example_genders = ["potato", "carrot", "broccoli", "male", "female", "super"]
 example_ethnicities = ["Latino", "White", "Red Martian", "White Martian", "Pan-Ethnic", "Sayan"]
 
+
+
+#creates 15000 santas
 15000.times do
-  santa = Santa.new(example_genders[rand(example_genders.length.to_i - 1)], example_ethnicities[rand(example_ethnicities.length.to_i - 1)], rand(140))
+  #adds random attributes to santa using rand method (Upper bounds for randomness set by length of respective array minus 1)
+  santa = Santa.new(example_genders[rand(example_genders.length.to_i - 1)], example_ethnicities[rand(example_ethnicities.length.to_i - 1)])
+  rand(140).times {santa.celebrate_birthday}
   p santa.age
   p santa.ethnicity
   p santa.gender
 end
+
+
