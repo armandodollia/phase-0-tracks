6995c206826f39fce824a2b35d8d3690d3766c1c
Armando Dollia <dollia.armando@gmail.com>
2017-01-19 16:22:32 -0600

    adds test code for attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -19,7 +19,17 @@ class Santa
 
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("Thin Mint")
+#santa = Santa.new
+#santa.speak
+#santa.eat_milk_and_cookies("Thin Mint")
 
+
+santas = []
+example_genders = ["potato", "carrot", "broccoli", "male", "female", "super"]
+example_ethnicities = ["Latino", "White", "Red Martian", "White Martian", "Pan-Ethnic", "Sayan"]
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i], i)
+end
+
+p santas
