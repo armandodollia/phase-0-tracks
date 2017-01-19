f0deea6178861b31f4c35b756ede8c42663058af
Armando Dollia <dollia.armando@gmail.com>
2017-01-19 15:56:15 -0600

    Tests and implements cookie and milk eating method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,7 +4,13 @@ class Santa
   puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
+  def eat_milk_and_cookies (cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
 
 end
 
 santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("Thin Mint")
