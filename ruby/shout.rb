237845dc65332c3c3e2aaae1acf169fa8f6e2716
Armando Dollia <dollia.armando@gmail.com>
2017-01-20 13:55:25 -0600

    Refactors and implements Shout as mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,27 @@
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-    "YAY!!! " + words + " :)"
+  def yell_happily(words)
+    puts "YAY!!! " + words + " :)"
   end
 end
 
+class Toddler
+  include Shout
+end
+
+class Significant_other
+  include Shout
+end
+
 #driver code
-puts Shout.yell_happily("We have ice cream")
-puts Shout.yell_angrily("You kick my dog")
+#puts Shout.yell_happily("We have ice cream")
+#puts Shout.yell_angrily("You kick my dog")
+
+johnny = Toddler.new
+johnny.yell_angrily("I WANT AN IPHONE")
+
+girlfriend = Significant_other.new
+girlfriend.yell_happily("Ice Cream")
