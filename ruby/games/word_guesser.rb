32a813fecb49d617513dc6fc78db6fd3d3bcebec
Armando Dollia <dollia.armando@gmail.com>
2017-01-22 20:38:59 -0600

    Finishes driver code and refactors class further

--- ruby/games/word_guesser.rb
+++ ruby/games/word_guesser.rb
@@ -1,5 +1,5 @@
 class Word_guesser
-  attr_reader :attempts, :word_length, :attempts, :guessed_letters_array
+  attr_reader :attempts, :word_length
 
   def initialize(word)
     @attempts = 0
@@ -8,14 +8,15 @@ class Word_guesser
     @guessed_letter = ""
     @word_length = word.length
     @guessed_letters_array = []
+    @word_length.times {@guessed_letters_array << "_"}
   end
 
   def matching_letters (guessed_letter)
-      @word_array.each do |letter|
-        if letter == guessed_letter
-          @guessed_letters_array << guessed_letter
-        else
-          @guessed_letters_array << "_"
+      @word_array.each_index do |index|
+        if @word_array[index] == guessed_letter
+          @guessed_letters_array[index] = guessed_letter
+        # else
+        #   @guessed_letters_array[index] = "_"
         end
       end
 
@@ -32,3 +33,24 @@ class Word_guesser
     end
   end
 end
+
+#driver code
+
+
+puts "Please enter a word to be guessed"
+guess_game = Word_guesser.new(gets.chomp)
+
+
+until guess_game.done_guessing || guess_game.attempts == guess_game.word_length
+  attempts_left = guess_game.word_length - guess_game.attempts
+  puts "Please enter a letter, you have #{attempts_left} attempts left"
+  puts guess_game.matching_letters(gets.chomp)
+
+  if guess_game.done_guessing && guess_game.attempts <= guess_game.word_length
+    puts "Congratulations on figuring it out!"
+  elsif !guess_game.done_guessing && guess_game.attempts >= guess_game.word_length
+      puts "YOU FAIL!"
+  end
+
+end
+
