70497ae6a7979cbde207bc9a23916790c05e72f5
Armando Dollia <dollia.armando@gmail.com>
2017-01-23 17:42:18 -0600

    Creates strReverse function

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,14 @@
 //store each value in a new string 
 //subtract 1 from i every iteration
 //end the loop when i = 0
+
+function strReverse(string){
+  var reverseString = "";
+
+  for(i = string.length-1, i >= 0, i--){
+    reverseString + string.charAt(i);
+  }
+
+  return reverseString;
+}
+
