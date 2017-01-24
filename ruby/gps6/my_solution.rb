07fcba1165c5ddcb3cd31e8b52a868f7d0f5583a
Armando Dollia <dollia.armando@gmail.com>
2017-01-23 23:49:23 -0600

    Fixes print code

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -74,20 +74,22 @@ end
  # initialize VirusPredictor for each state
 states = []
 
-STATE_DATA.each { |state, state_value| states << VirusPredictor.new(state, [state_value][:population_density], [state_value][:population]) }
+STATE_DATA.each { |state, state_value| states << VirusPredictor.new(state, state_value[:population_density], state_value[:population]) }
+
+states.each { |state| state.virus_effects}
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 #=======================================================================
 # Reflection Section
