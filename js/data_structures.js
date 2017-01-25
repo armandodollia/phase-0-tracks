0bcde1f5197acdfe8fe29e22611ff9ed611dd770
Armando Dollia <dollia.armando@gmail.com>
2017-01-25 17:14:05 -0600

    completes array modification

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "neon green", "mud pink", "potato brown"];
+var names = ["buster", "misty", "gideon", "bob"];
+
+colors.push("mustard gas yellow");
+names.push("cat");
+
+console.log(colors, names);
+
