2b99d945eadf918edb2ee5ab9dcc2a6f37fa18ec
Armando Dollia <dollia.armando@gmail.com>
2017-01-25 17:17:34 -0600

    Creates horses object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,8 +1,14 @@
 var colors = ["blue", "neon green", "mud pink", "potato brown"];
 var names = ["buster", "misty", "gideon", "bob"];
+var horses = {};
 
 colors.push("mustard gas yellow");
 names.push("cat");
 
 console.log(colors, names);
 
+for (var i = 0; i < colors.length; i++){
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);
