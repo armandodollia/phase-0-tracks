11024f0d9dfa7f8c02bcba6c154a70c62f31f64c
Armando Dollia <dollia.armando@gmail.com>
2017-02-02 22:18:37 -0600

    Implements database writing methods

--- databases/something_awesome/ledger.rb
+++ databases/something_awesome/ledger.rb
@@ -1,17 +1,36 @@
-# Create ledger class accepting name, item
 require 'sqlite3'
 
-# Creates ledger database
+def lend_item(ledger, item, name)
+  ledger.execute("INSERT INTO items (item, name, returned) VALUES (?, ?, ?)", [item, name, 'not returned'])
+end
+
+def return_item(ledger, item)
+  ledger.execute("UPDATE items SET returned='returned' WHERE item=(?)", [item])
+end
+
+# Creates ledger database and have it output results as a hash
 ledger = SQLite3::Database.new("ledger.db")
+ledger.results_as_hash = true
+
 
 # Creates table if it does not already exist
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS items(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
     item VARCHAR(255),
-    returned INT
+    name VARCHAR(255),
+    returned VARCHAR(255)
     )
 SQL
 
+#Creates the table
 ledger.execute(create_table_cmd)
+
+#ask user for the number of items they would like to add to their ledger
+# puts "How many items would you like to log?"
+# number_of_items = gets.chomp
+
+
+lend_item(ledger, "piano", "John")
+
+return_item(ledger, "piano")
