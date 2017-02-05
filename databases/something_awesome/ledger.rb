18a67fec1bf357adbd9f517994db569d20031376
Armando Dollia <dollia.armando@gmail.com>
2017-02-05 14:40:30 -0600

    Refactors database manipulation to a separate class

--- databases/something_awesome/ledger.rb
+++ databases/something_awesome/ledger.rb
@@ -1,47 +1,13 @@
 require 'sqlite3'
+require_relative 'ledger_manager'
 
-def lend_item(ledger, item, borrower)
-  ledger.execute("INSERT INTO items (item, borrower, returned) VALUES (?, ?, ?)", [item, borrower, 0])
-end
-
-def return_item(ledger, item)
-  ledger.execute("UPDATE items SET returned = ? WHERE item=?", [1, item])
-end
-
-def delete_row(ledger, item, borrower)
-  ledger.execute("DELETE FROM items WHERE item = ? AND borrower = ?", [item, borrower])
-end
-
-def delete_borrower(ledger, borrower)
-  ledger.execute("DELETE FROM items WHERE borrower = ?", [borrower])
-end
-
-def delete_item(ledger, item)
-  ledger.execute("DELETE FROM items WHERE item = ?", [item])
-end
-
-# Creates ledger database and have it output results as a hash
-ledger = SQLite3::Database.new("ledger.db")
-ledger.results_as_hash = true
-
-
-# Creates table if it does not already exist
-create_table_cmd = <<-SQL
-CREATE TABLE IF NOT EXISTS items(
-id INTEGER PRIMARY KEY,
-item VARCHAR(255),
-borrower VARCHAR(255),
-returned BOOLEAN
-)
-SQL
-
-#Creates the table
-ledger.execute(create_table_cmd)
+#instantiate Ledger_manager class
+ledger_manager = Ledger_manager.new
 
 rw_prompt = "" # Initialize loop break condition
 until rw_prompt == "exit" # Loop until the user says exit
+
 # Ask the user whether they would like to add items or view the current ledger
-#puts ""
 puts "\nType 'log' to add more items to ledger"
 puts "Type 'return' to set an item as returned"
 puts "Type 'view' to print the ledger"
@@ -65,55 +31,44 @@ if rw_prompt == "log"
       puts "Please enter the name of the borrower"
       borrower = gets.chomp
       #Store values in database
-      lend_item(ledger, item, borrower)
-  end
-
-elsif rw_prompt == "view"
-
-  # Store items table in a ledger
-  logs_table = ledger.execute("SELECT * FROM items")
-
-  # iterate through ledger and print items
-  logs_table.each do |log|
-    if log['returned'] == 1
-      puts "#{log['borrower']} has returned the #{log['item']}"
-    else
-      puts "#{log['borrower']} has not returned the #{log['item']}"
+      ledger_manager.lend_item(item, borrower)
     end
-  end
 
-elsif rw_prompt == "return"
-  puts "How many items are being returned?"
-  number_of_items = gets.chomp.to_i
-  number_of_items.times do
+  elsif rw_prompt == "view"
+    ledger_manager.print
+
+  elsif rw_prompt == "return"
+    puts "How many items are being returned?"
+    number_of_items = gets.chomp.to_i
+    number_of_items.times do
       # Get values from user
       puts "Please enter the item being returned"
       item = gets.chomp
       #set the item as returned
-      return_item(ledger, item)
+      ledger_manager.return_item(item)
     end
 
-elsif rw_prompt == 'delete row'
-  puts "Please type the name of the person you want to remove"
-  borrower = gets.chomp
-  puts "Please type the item they borrowed"
-  item = gets.chomp
-  delete_row(ledger, item, borrower)
+  elsif rw_prompt == 'delete row'
+    puts "Please type the name of the person you want to remove"
+    borrower = gets.chomp
+    puts "Please type the item they borrowed"
+    item = gets.chomp
+    ledger_manager.delete_row(item, borrower)
 
-elsif rw_prompt == 'delete borrower'
-  puts "Please type the name of the person you want to remove from the database"
-  borrower = gets.chomp
-  delete_borrower(ledger, borrower)
+  elsif rw_prompt == 'delete borrower'
+    puts "Please type the name of the person you want to remove from the database"
+    borrower = gets.chomp
+    ledger_manager.delete_borrower(borrower)
 
-elsif rw_prompt == 'delete item'
-  puts "Please type the name of the item you want to remove from the database"
-  item = gets.chomp
-  delete_item(ledger, item)
+  elsif rw_prompt == 'delete item'
+    puts "Please type the name of the item you want to remove from the database"
+    item = gets.chomp
+    ledger_manager.delete_item(item)
 
-elsif rw_prompt == 'exit'
-  puts "Thank you for using Armando's ledger program! :)"
+  elsif rw_prompt == 'exit'
+    puts "Thank you for using Armando's ledger program! :)"
 
-else
-  puts "ERROR! I do not recognize that input, please try again"
-end
+  else
+    puts "ERROR! I do not recognize that input, please try again"
+  end
 end
